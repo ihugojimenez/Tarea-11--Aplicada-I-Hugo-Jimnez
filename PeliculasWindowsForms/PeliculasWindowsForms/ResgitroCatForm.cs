d4639e186ec30d51a0ae6543a6ef14a44c065f54
ihugojimenez <hugofernando809@hotmail.com>
2016-06-16 11:39:46 -0400

    sigo...

--- PeliculasWindowsForms/PeliculasWindowsForms/ResgitroCatForm.cs
+++ PeliculasWindowsForms/PeliculasWindowsForms/ResgitroCatForm.cs
@@ -12,19 +12,67 @@ namespace PeliculasWindowsForms
 {
     public partial class ResgitroCatForm : Form
     {
-        
+        private PeliculasEntities db = new PeliculasEntities();
         public ResgitroCatForm()
         {
             InitializeComponent();
         }
 
+        private void Mostrar()
+        {
+            PeliculasCatListView.Items.Clear();
+
+            var query = from p in db.Movies
+                        select p;
+
+            foreach(var item in query)
+            {
+                ListViewItem ilv = PeliculasCatListView.Items.Add(item.CategoriaId.ToString());
+                ilv.SubItems.Add(item.Descripcion);
+            }
+        }
+
+        private void Agregar()
+        {
+            Movy m = new Movy()
+            {
+                CategoriaId = Convert.ToInt32(DesctextBoxT.Text),
+                Descripcion = DesctextBoxT.Text
+
+            };
+
+            db.Movies.Add(m);
+            db.SaveChanges();
+        }
+
+        private void Actualizar(int n)
+        {
+            Movy m = (from p in db.Movies
+                      where p.CategoriaId == n
+                      select p).FirstOrDefault();
+            m.Descripcion = DesctextBoxT.Text;
+            db.SaveChanges();
+        }
+
+        private void Eliminar(int n)
+        {
+            Movy mov = (from p in db.Movies
+                        where p.CategoriaId == n
+                        select p).FirstOrDefault();
+
+            db.Movies.Remove(mov);
+            db.SaveChanges();
+        }
+
         private void SaveButton_Click(object sender, EventArgs e)
         {
+            Agregar();
 
         }
 
         private void ResgitroCatForm_Load(object sender, EventArgs e)
         {
+            Mostrar();
 
         }
     }
